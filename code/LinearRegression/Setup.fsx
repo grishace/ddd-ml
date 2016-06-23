a6a550b878188628df77d074d77589e12b864374
Grigoriy Belenkiy <grishace@gmail.com>
2016-06-23 14:26:10 -0600

    Initial code samples

---
+++ code/LinearRegression/Setup.fsx
@@ -0,0 +1,10 @@
+﻿#I __SOURCE_DIRECTORY__
+#load "../packages/FSharp.Charting.0.90.14/FSharp.Charting.fsx"
+
+#r "../packages/FSharp.Data.2.3.1/lib/net40/FSharp.Data.dll"
+#r "../packages/Accord.3.1.0-alpha/lib/net45/Accord.dll"
+#r "../packages/Accord.Math.3.1.0-alpha/lib/net45/Accord.Math.dll"
+#r "../packages/Accord.Statistics.3.1.0-alpha/lib/net45/Accord.Statistics.dll"
+#r "../packages/Accord.Neuro.3.1.0-alpha/lib/net45/Accord.Neuro.dll"
+
+#time "on"
---
+++ code/NeuralNetwork/NeuralNetwork.fsx
@@ -0,0 +1,51 @@
+﻿#load "../LinearRegression/Setup.fsx"
+
+open System
+open FSharp.Data
+open FSharp.Data.CsvExtensions
+open Accord.Neuro
+open Accord.Neuro.Learning
+
+let train = CsvFile.Load("C:/DenverDevDay/MachineLearning/datasets/train.csv").Cache()
+
+let y = Array.ofSeq(seq { for r in train.Rows ->
+                                        let d = r.[0].AsFloat()
+                                        Array.ofSeq(seq { for i in 0 .. 9 -> if float i = d then 1.0 else 0.0 })})
+
+let x = Array.ofSeq(seq { for r in train.Rows ->
+                                        Array.ofSeq(seq { for i in 1 .. 784 -> r.[i].AsFloat() })})
+
+let v = Array.ofSeq(seq { for r in train.Rows -> r.[0].AsInteger() })
+
+open System.Drawing
+open FSharp.Charting
+
+let digit n = 
+    let points = 
+        [|
+            for i in 0 .. 27 do
+                    for j in 0 .. 27 do
+                        let idx = i * 28 + j
+                        if x.[n].[idx] <> 0.0 then yield (j, -i) 
+        |]
+    Chart.Point(points).WithMarkers(Size=15,Color=Color.Black)
+
+
+let network = ActivationNetwork(IdentityFunction(), 784, [| 5; 7; 10 |])
+let teacher = new ParallelResilientBackpropagationLearning(network)
+
+let rec run (perr:float) (err:float) =
+  let aerr = Math.Abs(perr-err)
+  printfn "%f" aerr
+  if aerr < 0.000001 * perr then () else run err (teacher.RunEpoch(x, y))  
+
+run 0.0 Double.PositiveInfinity
+
+let maxi(a) = 
+  let m = Array.max a
+  Array.findIndex (fun i -> i = m) a
+
+let mutable c = 0
+for i in 0..x.Length-1 do
+    let n = maxi(network.Compute(x.[i]))
+    if v.[i] = n then c <- c + 1 else ()
