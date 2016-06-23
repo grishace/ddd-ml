96633cfaf3d414069ba28896e342e7ded81927b7
Grigoriy Belenkiy <grishace@gmail.com>
2016-06-23 16:24:21 -0600

    Split log line(s)

--- code/LogisticRegression/LogisticRegression.fsx
+++ code/LogisticRegression/LogisticRegression.fsx
@@ -15,7 +15,9 @@ let XTitle = "Exam 1 Score"
 [<Literal>]
 let YTitle = "Exam 2 Score"
 
-let separate f v = Array.ofSeq(Seq.map (fun t -> snd t) (Seq.filter f (seq { for r in data.Rows -> (r.Admitted, v(r)) })))
+let separate f v = 
+    let filter = Seq.filter f (seq { for r in data.Rows -> (r.Admitted, v(r)) })
+    Array.ofSeq(Seq.map (fun t -> snd t) filter)
 
 let fa x = fst x
 let ex1a = separate fa (fun r -> float r.Exam1)
@@ -48,7 +50,8 @@ let bx = [| Array.min ex1-2.0; Array.max ex1+2.0 |]
 Chart.Combine([
                 Chart.Point(Array.zip ex1a ex2a).WithStyling(Name="Admitted",Color=Color.DarkGreen)
                 Chart.Point(Array.zip ex1na ex2na).WithStyling(Name="Not admitted",Color=Color.Red)
-                Chart.Line([ for x in bx -> (x, (-1.0/coeff.[2]) * (coeff.[1]*x + coeff.[0]))]).WithStyling(Name="Decision boundary",Color=Color.Navy)
+                Chart.Line([ for x in bx -> (x, (-1.0/coeff.[2]) * (coeff.[1]*x + coeff.[0]))])
+                    .WithStyling(Name="Decision boundary",Color=Color.Navy)
             ])
             .WithXAxis(Title=XTitle,Min=Array.min ex1-1.0,Max=Array.max ex1+1.0)
             .WithYAxis(Title=YTitle,Min=Array.min ex2-1.0,Max=Array.max ex2+1.0)
